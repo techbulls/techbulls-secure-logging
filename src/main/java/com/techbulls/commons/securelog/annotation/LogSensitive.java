8352c567086affd921da4097368e8c9039c84a7c
Kalpak Gadre <kalpak.gadre@techbulls.com>
2022-07-15 12:46:44 +0530

    Initial code

---
+++ src/main/java/com/techbulls/commons/securelog/annotation/LogSensitive.java
@@ -0,0 +1,8 @@
+package com.techbulls.commons.securelog.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+public @interface LogSensitive {
+}
---
+++ src/main/java/com/techbulls/commons/securelog/annotation/SecureLog.java
@@ -0,0 +1,8 @@
+package com.techbulls.commons.securelog.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface SecureLog {
+}
